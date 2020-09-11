584c162bfde2a93d7a9779a00a1ccab899d5cc29
Andriej10 <andriejoni10@gmail.com>
2020-09-11 10:19:16 +0700

    Update Code

--- app/src/main/java/stickearn/movie/stickearnmovieapps/view/movieHome/HomeMovieActivity.kt
+++ app/src/main/java/stickearn/movie/stickearnmovieapps/view/movieHome/HomeMovieActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.paging.PagedList
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_home_movie.*
 import kotlinx.android.synthetic.main.layout_home_now_playing_movies.*
@@ -13,6 +14,7 @@ import kotlinx.android.synthetic.main.layout_home_popular_movies.*
 import kotlinx.android.synthetic.main.layout_home_top_rated_movies.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import stickearn.movie.stickearnmovieapps.R
+import stickearn.movie.stickearnmovieapps.data.MovieData
 import stickearn.movie.stickearnmovieapps.utils.PaginationStatus
 import stickearn.movie.stickearnmovieapps.view.movieDetails.DetailMovieActivity
 import stickearn.movie.stickearnmovieapps.view.movieFavorite.FavoriteMovieActivity
@@ -47,11 +49,10 @@ class HomeMovieActivity : AppCompatActivity() {
         }
     }
 
-    private var movieListener = OnMovieListener {
-        homeMovieViewModel.movieClicked(it)
-    }
+    private var movieListener = movieListener()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override
+    fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_movie)
         initView()
@@ -73,71 +74,35 @@ class HomeMovieActivity : AppCompatActivity() {
     private fun initObserver() {
 
         homeMovieViewModel.initializePopularMoviesLiveData().observe(this, {
-
-            if (pbPopularMovies.isVisible) pbPopularMovies.isVisible = false
-            if (!rvPopularMovies.isVisible) rvPopularMovies.isVisible = true
-
-            popularMoviesAdapter.submitList(it)
+            renderPopularMovieData(it)
         })
 
         homeMovieViewModel.initializeTopRatedMoviesLiveData().observe(this, {
-
-            if (pbTopRatedMovies.isVisible) pbTopRatedMovies.isVisible = false
-            if (!rvTopRatedMovies.isVisible) rvTopRatedMovies.isVisible = true
-
-            topRatedMoviesAdapter.submitList(it)
+            renderTopRatedMovieData(it)
         })
 
         homeMovieViewModel.initializeNowPlayingMoviesLiveData().observe(this, {
-
-            if (pbNowPlayingMovies.isVisible) pbNowPlayingMovies.isVisible = false
-            if (!rvNowPlayingMovies.isVisible) rvNowPlayingMovies.isVisible = true
-
-            nowPlayingMoviesAdapter.submitList(it)
+            renderNowPlayingMovieData(it)
         })
 
         homeMovieViewModel.popularMoviesDataSourceFactory?.paginationStatus?.observe(this, {
-            when (it) {
-                is PaginationStatus.Error -> {
-                    rvPopularMovies.isVisible = false
-                    pbPopularMovies.isVisible = false
-                    btnRefreshPopularMovies.isVisible = true
-                }
-            }
+            checkPopularMoviesPaginationStatus(it)
         })
 
         homeMovieViewModel.topRatedMoviesDataSourceFactory?.paginationStatus?.observe(this, {
-            when (it) {
-                is PaginationStatus.Error -> {
-
-                    rvTopRatedMovies.isVisible = false
-                    pbTopRatedMovies.isVisible = false
-                    btnRefreshTopRatedMovies.isVisible = true
-                }
-            }
+            checkTopRatedMoviesPaginationStatus(it)
         })
 
         homeMovieViewModel.nowPlayingMoviesDataSourceFactory?.paginationStatus?.observe(this, {
-            when (it) {
-                is PaginationStatus.Error -> {
-
-                    rvNowPlayingMovies.isVisible = false
-                    pbNowPlayingMovies.isVisible = false
-                    btnRefreshNowPlayingMovies.isVisible = true
-                }
-            }
+            checkNowPlayingMoviesPaginationStatus(it)
         })
 
         homeMovieViewModel.goToDetailMovieEvent.observe(this, {
-
-            val intent = Intent(this, DetailMovieActivity::class.java)
-            intent.putExtra(DetailMovieActivity.MOVIE_DATA, it)
-
-            startActivity(intent)
+            goToDetailMovieEvent(it)
         })
 
         homeMovieViewModel.goToFavoriteActivityEvent.observe(this, {
-            startActivity(Intent(this, FavoriteMovieActivity::class.java))
+            goToFavoriteMovieEvent()
         })
     }
 
@@ -148,35 +113,132 @@ class HomeMovieActivity : AppCompatActivity() {
         nowPlayingMoviesAdapter.onMovieListener = movieListener
 
         toolbar.menu.findItem(R.id.menuFavoriteHome).setOnMenuItemClickListener {
-            homeMovieViewModel.favoriteIconClicked()
+            favoriteIconClicked()
             true
         }
 
         btnRefreshPopularMovies.setOnClickListener {
-
-            btnRefreshPopularMovies.isVisible = false
-            pbPopularMovies.isVisible = true
-            rvPopularMovies.isVisible = false
-
-            homeMovieViewModel.refreshPopularMovie()
+            refreshPopularMovie()
         }
 
         btnRefreshTopRatedMovies.setOnClickListener {
-
-            btnRefreshTopRatedMovies.isVisible = false
-            pbTopRatedMovies.isVisible = true
-            rvTopRatedMovies.isVisible = false
-
-            homeMovieViewModel.refreshTopRatedMovie()
+            refreshTopRatedMovie()
         }
 
         btnRefreshNowPlayingMovies.setOnClickListener {
+            refreshNowPlayingMovie()
+        }
+    }
 
-            btnRefreshNowPlayingMovies.isVisible = false
-            pbNowPlayingMovies.isVisible = true
-            rvNowPlayingMovies.isVisible = false
+    private fun movieListener(): OnMovieListener {
+        return OnMovieListener {
+            moviesClicked(it)
+        }
+    }
 
-            homeMovieViewModel.refreshNowPlayingMovie()
+    private fun favoriteIconClicked() {
+        homeMovieViewModel.favoriteIconClicked()
+    }
+
+    private fun moviesClicked(movieData: MovieData) {
+        homeMovieViewModel.movieClicked(movieData)
+    }
+
+    private fun goToDetailMovieEvent(movieData: MovieData) {
+
+        val intent = Intent(this, DetailMovieActivity::class.java)
+        intent.putExtra(DetailMovieActivity.MOVIE_DATA, movieData)
+
+        startActivity(intent)
+    }
+
+    private fun goToFavoriteMovieEvent() {
+        startActivity(Intent(this, FavoriteMovieActivity::class.java))
+    }
+
+    private fun refreshPopularMovie() {
+
+        btnRefreshPopularMovies.isVisible = false
+        pbPopularMovies.isVisible = true
+        rvPopularMovies.isVisible = false
+
+        homeMovieViewModel.refreshPopularMovie()
+    }
+
+    private fun refreshTopRatedMovie() {
+
+        btnRefreshTopRatedMovies.isVisible = false
+        pbTopRatedMovies.isVisible = true
+        rvTopRatedMovies.isVisible = false
+
+        homeMovieViewModel.refreshTopRatedMovie()
+    }
+
+    private fun refreshNowPlayingMovie() {
+
+        btnRefreshNowPlayingMovies.isVisible = false
+        pbNowPlayingMovies.isVisible = true
+        rvNowPlayingMovies.isVisible = false
+
+        homeMovieViewModel.refreshNowPlayingMovie()
+    }
+
+    private fun renderPopularMovieData(movieDatas: PagedList<MovieData>) {
+
+        if (pbPopularMovies.isVisible) pbPopularMovies.isVisible = false
+        if (!rvPopularMovies.isVisible) rvPopularMovies.isVisible = true
+
+        popularMoviesAdapter.submitList(movieDatas)
+    }
+
+    private fun renderTopRatedMovieData(movieDatas: PagedList<MovieData>) {
+
+        if (pbTopRatedMovies.isVisible) pbTopRatedMovies.isVisible = false
+        if (!rvTopRatedMovies.isVisible) rvTopRatedMovies.isVisible = true
+
+        topRatedMoviesAdapter.submitList(movieDatas)
+    }
+
+    private fun renderNowPlayingMovieData(movieDatas: PagedList<MovieData>) {
+
+        if (pbNowPlayingMovies.isVisible) pbNowPlayingMovies.isVisible = false
+        if (!rvNowPlayingMovies.isVisible) rvNowPlayingMovies.isVisible = true
+
+        nowPlayingMoviesAdapter.submitList(movieDatas)
+    }
+
+    private fun checkPopularMoviesPaginationStatus(paginationStatus: PaginationStatus) {
+
+        when (paginationStatus) {
+            is PaginationStatus.Error -> {
+                rvPopularMovies.isVisible = false
+                pbPopularMovies.isVisible = false
+                btnRefreshPopularMovies.isVisible = true
+            }
+        }
+    }
+
+    private fun checkTopRatedMoviesPaginationStatus(paginationStatus: PaginationStatus) {
+
+        when (paginationStatus) {
+            is PaginationStatus.Error -> {
+
+                rvTopRatedMovies.isVisible = false
+                pbTopRatedMovies.isVisible = false
+                btnRefreshTopRatedMovies.isVisible = true
+            }
+        }
+    }
+
+    private fun checkNowPlayingMoviesPaginationStatus(paginationStatus: PaginationStatus) {
+
+        when (paginationStatus) {
+            is PaginationStatus.Error -> {
+
+                rvNowPlayingMovies.isVisible = false
+                pbNowPlayingMovies.isVisible = false
+                btnRefreshNowPlayingMovies.isVisible = true
+            }
         }
     }
 }
